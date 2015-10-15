726dd4373f9ee3d418386a17e4549ec57b432aa1
Leonard Botha <bzl@dell-xps>
2015-10-15 17:23:48 +0200

    more refactoring

--- include/mesh.h
+++ include/mesh.h
@@ -57,8 +57,8 @@ class mesh {
         void walkable_by_gradient(float gradient);
         void walkable_by_weighted_gradient(float radius, float gradient, int weighting = 1);
 
-        void remove_overhangs(float height, float step_height, float radius);
         void remove_bottlenecks(float step_height, float radius);
+        void remove_overhangs(float height, float step_height, float radius);
 
         void graph_rejoin(float step_distance);
         void proximity_rejoin(float step_distance);
--- src/main.cpp
+++ src/main.cpp
@@ -4,45 +4,26 @@
 
 #include "../include/mesh.h"
 
-void do_stuff(mesh & m, float angle, float dist, std::string filepath, std::string filename) {
-    using namespace std;
-    m.walkable_by_gradient(6*0.0872664626f);
-    // cout << m.prune_weighted_gradient(0.3f, 6*0.0872664626f, 30) << endl;
-
-    // m.prune_bottlenecks(0.3f, 0.5f);
-    // m.prune_overhangs(20.0f, 0.5f, 0.05f);
-
-    // cout << m.rejoin_chunks(dist) << endl;
-    // cout << m.keep_largest_chunk() << endl;
-
-    // m.merge_chunks(0.5f);
-    cout << m.export_to_file("obj", filepath, filename) << endl;
-    m.clear_walkable_surfaces();
-}
 
 int main() {
     using namespace std;
 
     mesh m;
     cout << m.import_from_file("meshes/petra/Buildings/Wadi_Farasa/Whole_Area/Wadi_Model_GPS_with_constant.obj") << endl;
-    // cout << m.import_from_file("meshes/overhang.obj") << endl;
-    // cout << m.setup_neighbouring_triangles() << endl;
-    // m.setup_spatial_structures();
-    // cout << m.prune(0.523599f) << endl;
-    // m.prune_bottlenecks(0.4f, 0.8f);
+    m.setup_neighbouring_triangles();
+    m.setup_spatial_structures();
 
-    // cout << m.setup_neighbouring_triangles() << endl;
-    float d5 = 0.0872664626f;
-    float j01 = 0.1f;
+    // m.walkable_by_gradient(6*0.0872664626f);
+    m.walkable_by_weighted_gradient(0.3f, 6*0.0872664626f, 30);
 
-    for (int j = 4; j <= 4; ++j){
-        for (int a = 8; a <= 8; ++ a){
-            string filename = "a"+ to_string(5*a) + "_j" + ("0-"+to_string(j)) + ".obj";
-            do_stuff(m, d5*4, j01 * j, "meshes/euclidian_distance/", filename);
-            // string filename = "test.obj";
-            // do_stuff(m, d5*4, j01 * j, "meshes/", filename);
-        }
-    }
+    // m.remove_bottlenecks(0.3f, 0.5f);
+    // m.remove_overhangs(20.0f, 0.5f, 0.05f);
+
+    // m.remove_regions(5);
+    m.remove_regions(20.0f);
+    // m.keep_largest_region();
+
+    cout << m.export_to_file("obj", "meshes/euclidian_distance/", "test.obj") << endl;
 
     return 0;
 }
